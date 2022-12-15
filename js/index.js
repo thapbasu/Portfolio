3908a6cee4b030d681f351591adaa72e5c44b386
Basu Thapa <thapabasu000@gmail.com>
2022-12-15 05:51:17 +0545

    add about me section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const pEL = document.querySelector(".p-red")
+
 const prog = "Front End Developer"
 let index = 1;
 setInterval(writeText,100)
@@ -6,4 +7,20 @@ setInterval(writeText,100)
 function writeText(){
     pEL.innerText = prog.slice(0,index)
     index++
+}
+
+const tablelinks = document.getElementsByClassName("table-links")
+const tabcontents = document.getElementsByClassName("tab-contents")
+
+function openTab(tabname){
+    for(tablelink of tablelinks){
+        tablelink.classList.remove("active-link");
+
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+        
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
 }
